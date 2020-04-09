cf8c5e265d7825df86165bc1e9843d2623ad577e
Franchesca Leung <franchescacarina@yahoo.com>
2020-04-09 16:25:28 -0700

    lecture changes

--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -1,10 +1,21 @@
 package tudelft.gettingstarted;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class GettingStartedTest {
+	
+	GettingStarted g;
+	
+	@BeforeEach // usually used for initialization so this will happen before each test case
+	public void setup() {
+		g = new GettingStarted();
+	}
 
+	//@Disabled //turns off a test case, skips it
+	// @Timeout limits performance and will show error if code doesnt happen within a specified time
     @Test
     public void addFiveTo20() {
         int result = new GettingStarted().addFive(20);
@@ -13,15 +24,15 @@ public class GettingStartedTest {
 
 //    UNCOMMENT THE CODE BELOW, AND FILL THE GAPS!
 
-//    @Test
-//    public void addFiveToZero() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(???, result);
-//    }
-//
-//    @Test
-//    public void addFiveToMinus20() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(????,result);
-//    }
+    @Test
+    public void addFiveToZero() {
+        int result = new GettingStarted().addFive(0);
+        Assertions.assertEquals(5, result);
+    }
+
+    @Test
+    public void addFiveToMinus20() {
+        int result = new GettingStarted().addFive(-20);
+        Assertions.assertEquals(-15,result);
+    }
 }
